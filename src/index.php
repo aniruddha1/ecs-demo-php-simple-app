18b4e93a9e5646e36723f56bfcca4db9f355cf92
Ani G <aniruddha.godawale@gmail.com>
2018-06-21 09:28:05 +0100

    Update index.php

--- src/index.php
+++ src/index.php
@@ -15,9 +15,9 @@
     <body>
         <div class="container">
             <div class="hero-unit">
-                <h1>Simple PHP App</h1>
-                <h2>Congratulations</h2>
-                <p>Your PHP application is now running on a container in Amazon ECS.Hello Aniruddh</p>
+                <h1>AWS ECS DevOps Demo</h1>
+                <h2>Hello Cognizant</h2>
+                <p>Your PHP application is now running on a container in Amazon ECS</p>
                 <p>The container is running PHP version <?php echo phpversion(); ?>.</p>
                 <?php
                         $myfile = fopen("/var/www/my-vol/date", "r") or die("");
